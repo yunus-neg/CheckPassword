e3d5f16d145fcfac44fbc05d1f4efb493bef370d
yunus-neg <35871957+yunus-neg@users.noreply.github.com>
2018-03-13 00:23:55 +0300

    .

--- index.js
+++ index.js
@@ -28,21 +28,33 @@ function PasswordCheck() {
 
 }
 
-let PasswordStrength=document.getElementById("PasswordStrength");
+let PasswordStrength = document.getElementById("PasswordStrength");
+let PasswordField = document.getElementById("PasswordField");
+let AdvancedMode = document.getElementById("AdvancedMode");
 
 function calculatePasswordStrength(password) {
-console.log(password.length);
+    password = password.value;
+    console.log(password.length);
     //total score of password
     let iPasswordScore = 0;
 
-    if (password.length < 8){
-    PasswordStrength.innerHTML= 0;
-    return;
+
+
+    if (password.length < 8) {
+        PasswordStrength.textContent = "Password Strength: " + 0;
+        return;
     }
     else if (password.length >= 10)
         iPasswordScore += 2;
     else
         iPasswordScore += 1;
+
+    if (AdvancedMode.checked) {
+        if (CommonPassword(password)) {
+            PasswordStrength.textContent = "Password Strength: " + 0;
+            return;
+        }
+    }
 
     //if it contains one digit, add 2 to total score
     if (password.match("(?=.*[0-9]).*"))
@@ -60,6 +72,28 @@ console.log(password.length);
     if (password.match("(?=.*[~!@#$%^&*()_-]).*"))
         iPasswordScore += 2;
 
-        PasswordStrength.innerHTML= iPasswordScore;
+    PasswordStrength.textContent = "Password Strength: " + iPasswordScore;
 
+}
+
+
+function CommonPassword(Password) {
+
+    let CommonPasswords = require('./CommonPasswords.json'); //(with path)
+    let count =0
+    for (const index in CommonPasswords) {
+        console.log(CommonPasswords[index].length);
+        if(CommonPasswords[index].length<8){
+            console.log(CommonPasswords[index]+"---"+CommonPasswords[index].length);
+            count++;
+        }
+        if (CommonPasswords[index] == Password) {
+            console.log("matches");
+            return true;
+        }
+        // console.log(CommonPasswords[index]);
+    }
+    console.log("less than 8 letters"+count);
+    return false;
+   
 }
