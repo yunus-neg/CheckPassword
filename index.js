02f1da76e6239caf7d30dfd5dd6b267ceae341d7
Basil <mr.albrrak@gmail.com>
2018-03-19 22:27:06 +0300

    Fix the progress bar

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let FoundInCommonpassword=false;
+let FoundInCommonpassword = false;
 
 let PasswordStrength = document.getElementById("PasswordStrength");
 let PasswordField = document.getElementById("PasswordField");
@@ -10,115 +10,147 @@ let LowerCaseLetter = document.getElementById("LowerCaseLetter");
 let UpperCaseLetter = document.getElementById("UpperCaseLetter");
 let SpecialCharacter = document.getElementById("SpecialCharacter");
 
-let WarningBox=document.getElementById("WarningBox");
+let WarningBox = document.getElementById("WarningBox");
 let Warning = document.getElementById("Warning");
 let CommonPasswordText = document.getElementById("CommonPassword");
 
 function calculatePasswordStrength(password) {
-    CommonPasswordText.innerHTML = "";
-    //total score of password
-    let PasswordScore = 1;
+  CommonPasswordText.innerHTML = "";
+  //total score of password
+  let PasswordScore = 1;
 
 
 
-    CountPassword(password);
+  CountPassword(password);
 
-    if (password.length < 8) {
-        PasswordStrength.innerHTML = "Password Strength: " + 0;
-        progressBar.style.width = "0%";
-        return;
-    }
-    else if (password.length >= 10)
-        PasswordScore += 1;
-    
+  if (password.length < 8) {
+    PasswordStrength.innerHTML = "Password Strength: " + 0;
+    progressBar.style.width = "0%";
+    return;
+  } else if (password.length >= 10)
+    PasswordScore += 1;
 
 
-    if (FoundInCommonpassword) {
-        PasswordScore--;
-    }
+
+  if (FoundInCommonpassword) {
+    PasswordScore--;
+  }
 
 
-    //if it contains one digit, add 2 to total score
-    if (password.match("(?=.*[0-9]).*"))
-        PasswordScore += 2;
+  //if it contains one digit, add 2 to total score
+  if (password.match("(?=.*[0-9]).*"))
+    PasswordScore += 2;
 
-    //if it contains one lower case letter, add 2 to total score
-    if (password.match("(?=.*[a-z]).*"))
-        PasswordScore += 2;
+  //if it contains one lower case letter, add 2 to total score
+  if (password.match("(?=.*[a-z]).*"))
+    PasswordScore += 2;
 
-    //if it contains one upper case letter, add 2 to total score
-    if (password.match("(?=.*[A-Z]).*"))
-        PasswordScore += 2;
+  //if it contains one upper case letter, add 2 to total score
+  if (password.match("(?=.*[A-Z]).*"))
+    PasswordScore += 2;
 
-    //if it contains one special character, add 2 to total score
-    if (password.match("(?=.*[~!@#$%^&*()_-]).*"))
-        PasswordScore += 2;
+  //if it contains one special character, add 2 to total score
+  if (password.match("(?=.*[~!@#$%^&*()_-]).*"))
+    PasswordScore += 2;
 
-    PasswordStrength.innerHTML = "Password Strength: " + PasswordScore;
-    progressBar.style.width = PasswordScore * 10 + "%";
-    progressBar.style.backgroundColor="rgb("+(255-(Math.floor(2.56*(10*PasswordScore)))) +","+Math.floor(2.56*(10*PasswordScore))+",0)";
-    console.log(255-(Math.floor(2.56*(10*PasswordScore))));
+  PasswordStrength.innerHTML = "Password Strength: " + PasswordScore;
+  progressBar.style.width = PasswordScore * 10 + "%";
+  progressBar.className = 'determinate';
+  switch (PasswordScore) {
+    case 1:
+      progressBar.className += ' red darken-4';
+      break;
+    case 2:
+      progressBar.className += ' red darken-3';
+      break;
+    case 3:
+      progressBar.className += ' red darken-2';
+      break;
+    case 4:
+      progressBar.className += ' orange darken-3';
+      break;
+    case 5:
+      progressBar.className += ' orange darken-2';
+      break;
+    case 6:
+      progressBar.className += ' orange darken-1';
+      break;
+    case 7:
+    progressBar.className += ' green lighten-2';
+      break;
+    case 8:
+      progressBar.className += ' green';
+      break;
+    case 9:
+      progressBar.className += ' green darken-1';
+      break;
+    case 10:
+      progressBar.className += ' green darken-3';
+      break;
+    default:
+
+  };
 }
 
 
 function CommonPassword(Password) {
 
-    let CommonPasswords = require('./CommonPasswords.json');
-    for (const index in CommonPasswords) {
-        if (CommonPasswords[index] == Password) {
-            console.log("matches");
-            FoundInCommonpassword=true;
-            return;
-        }
+  let CommonPasswords = require('./CommonPasswords.json');
+  for (const index in CommonPasswords) {
+    if (CommonPasswords[index] == Password) {
+      console.log("matches");
+      FoundInCommonpassword = true;
+      return;
     }
-    FoundInCommonpassword=false;
+  }
+  FoundInCommonpassword = false;
 
 }
 
 function CountPassword(Password) {
-    let DigitCount = 0;
-    let LowerCaseLetterCount = 0;
-    let UpperCaseLetterCount = 0;
-    let SpecialCharacterCount = 0;
+  let DigitCount = 0;
+  let LowerCaseLetterCount = 0;
+  let UpperCaseLetterCount = 0;
+  let SpecialCharacterCount = 0;
 
-    for (const index in Password) {
-        if (Password[index].match("(?=.*[0-9]).*"))
-            DigitCount++;
+  for (const index in Password) {
+    if (Password[index].match("(?=.*[0-9]).*"))
+      DigitCount++;
 
-        else if (Password[index].match("(?=.*[a-z]).*"))
-            LowerCaseLetterCount++;
+    else if (Password[index].match("(?=.*[a-z]).*"))
+      LowerCaseLetterCount++;
 
-        else if (Password[index].match("(?=.*[A-Z]).*"))
-            UpperCaseLetterCount++;
+    else if (Password[index].match("(?=.*[A-Z]).*"))
+      UpperCaseLetterCount++;
 
-        else if (Password[index].match("(?=.*[~!@#$%^&*()_-]).*"))
-            SpecialCharacterCount++;
-    }
+    else if (Password[index].match("(?=.*[~!@#$%^&*()_-]).*"))
+      SpecialCharacterCount++;
+  }
 
-    Digit.innerHTML = "Digits: " + DigitCount;
-    LowerCaseLetter.innerHTML = "lower case letteres: " + LowerCaseLetterCount;
-    UpperCaseLetter.innerHTML = "Upper case letteres: " + UpperCaseLetterCount;
-    SpecialCharacter.innerHTML = "Special Characteres: " + SpecialCharacterCount;
+  Digit.innerHTML = "Digits: " + DigitCount;
+  LowerCaseLetter.innerHTML = "lower case letteres: " + LowerCaseLetterCount;
+  UpperCaseLetter.innerHTML = "Upper case letteres: " + UpperCaseLetterCount;
+  SpecialCharacter.innerHTML = "Special Characteres: " + SpecialCharacterCount;
 
-    CommonPassword(Password);
+  CommonPassword(Password);
 
-    if (DigitCount < 1 || LowerCaseLetterCount < 1 || UpperCaseLetterCount < 1 || SpecialCharacterCount < 1 || Password.length<8)
-        WarningCheck(DigitCount, LowerCaseLetterCount, UpperCaseLetterCount, SpecialCharacterCount,Password);
-    else{
-        WarningBox.hidden=true;
-    }
+  if (DigitCount < 1 || LowerCaseLetterCount < 1 || UpperCaseLetterCount < 1 || SpecialCharacterCount < 1 || Password.length < 8)
+    WarningCheck(DigitCount, LowerCaseLetterCount, UpperCaseLetterCount, SpecialCharacterCount, Password);
+  else {
+    WarningBox.hidden = true;
+  }
 }
 
-function WarningCheck(DigitCount, LowerCaseLetterCount, UpperCaseLetterCount, SpecialCharacterCount,Password) {
-    WarningBox.hidden=false;
-    let NewWarning = "Warning<br>";
+function WarningCheck(DigitCount, LowerCaseLetterCount, UpperCaseLetterCount, SpecialCharacterCount, Password) {
+  WarningBox.hidden = false;
+  let NewWarning = "Warning<br>";
 
-    Password.length <8 ?NewWarning+="Less than 8 charcters<br>":"";
-    FoundInCommonpassword ? NewWarning+="Common Passwrod<br>" :"";
-    DigitCount < 1 ? NewWarning += "digits<br>" : "";
-    LowerCaseLetterCount < 1 ? NewWarning += "Lower Case Letter<br>" : "";
-    UpperCaseLetterCount < 1 ? NewWarning += "Upper Case Letter<br>" : "";
-    SpecialCharacterCount < 1 ? NewWarning += "Special Character<br>" : "";
+  Password.length < 8 ? NewWarning += "Less than 8 charcters<br>" : "";
+  FoundInCommonpassword ? NewWarning += "Common Passwrod<br>" : "";
+  DigitCount < 1 ? NewWarning += "digits<br>" : "";
+  LowerCaseLetterCount < 1 ? NewWarning += "Lower Case Letter<br>" : "";
+  UpperCaseLetterCount < 1 ? NewWarning += "Upper Case Letter<br>" : "";
+  SpecialCharacterCount < 1 ? NewWarning += "Special Character<br>" : "";
 
-    Warning.innerHTML = NewWarning;
+  Warning.innerHTML = NewWarning;
 }
